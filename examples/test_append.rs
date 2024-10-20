336e08e72f9604570f0ebff5a0c5701dd6edac0f
Dumpinground <e605336584@icloud.com>
2024-10-20 20:56:44 +0800

    update

--- examples/test_append.rs
+++ examples/test_append.rs
@@ -1,10 +1,7 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let resp = reqwest::blocking::get("https://gist.githubusercontent.com/Dumpinground/5f10045365df9e0c6406308675a01157/raw/b564cb155e89bf7fd7028284e3dcdfd190b65d82/text.txt").expect("request failed");
-    let text = resp.text().expect("body invalid");
-    // let text = read_to_string("text.txt").unwrap();
-    println!("{}", text);
+    let text = read_to_string("text.txt").unwrap();
     let mut result = String::new();
     for t in text.chars() {
         if t == '\n' {
