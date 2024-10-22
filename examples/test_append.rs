a6f6cafcaca201659150172375ebb636497840a3
Dumpinground <e605336584@icloud.com>
2024-10-22 18:17:37 +0800

    update

--- examples/test_append.rs
+++ examples/test_append.rs
@@ -2,12 +2,27 @@ use std::fs::read_to_string;
 
 fn main() {
     let text = read_to_string("text.txt").unwrap();
+    append_postfix(&text);
+}
+
+fn append_postfix(text: &str) -> String {
     let mut result = String::new();
+    let s = "\r\n-";
     for t in text.chars() {
-        if t == '\n' {
-            result += "}'";
+        if s.contains(t) {
+            result += "↙";
         }
         result += &String::from(t);
-        println!("{result}");
+        println!("{}", result.lines().last().unwrap());
     }
+    result
+}
+
+#[test]
+fn text_compare() {
+    let text1 = read_to_string("text.txt").unwrap();
+    let text2 = "state = :state
+state - a = :state
+";
+    assert_eq!(text1, text2);
 }
